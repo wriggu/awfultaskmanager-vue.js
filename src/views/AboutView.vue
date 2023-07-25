affb85bbd47141fdfde05bbc1d52c0a71f1f05de
Dan Qu <130500229+wriggu@users.noreply.github.com>
2023-07-25 14:31:54 +0900

    LGTM! :)

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,8 +2,7 @@
   <div class="about">
     <h1 style="margin-bottom: 20px;">About This Project</h1>
     <p style="margin-bottom: 50px;">
-      This is a project is based on the John Komarnicki's Vue 3 Crash Course
-      project.
+      This project is created by Dan Qu - based on John Komarnicki's Vue 3 Crash Course project.
     </p>
     <a
       href="https://www.youtube.com/watch?v=KTFH4P8unUQ"
