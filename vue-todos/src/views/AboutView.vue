dacb835382147e6582f8d73ae9a0882719933c6e
wriggu <wrigwithao@gmail.com>
2023-07-25 13:45:31 +0900

    v1 init

---
+++ vue-todos/src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1 style="margin-bottom: 20px;">About This Project</h1>
+    <p style="margin-bottom: 50px;">
+      This is a project is based on the John Komarnicki's Vue 3 Crash Course
+      project.
+    </p>
+    <a
+      href="https://www.youtube.com/watch?v=KTFH4P8unUQ"
+      >Watch here</a
+    >
+  </div>
+</template>
+
+<style>
+.about {
+  text-align: center;
+  padding: 50px;
+  margin: 20px;
+}
+</style>
